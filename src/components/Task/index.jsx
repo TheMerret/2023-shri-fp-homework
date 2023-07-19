14f8e937ebdc1947a1d28a40282f26a0ebabf853
TheMerret <merret@bk.ru>
2023-07-19 23:03:15 +0300

    feat: add linters

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
+const Task = ({ title, description, children }) => {
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.title}>{title}</h2>
 
-const Task = ({title, description, children}) => {
-    return (
-        <div className={styles.root}>
-            <h2 className={styles.title}>
-                {title}
-            </h2>
+      <p>{description}</p>
 
-            <p>{description}</p>
-
-            {children}
-        </div>
-    );
+      {children}
+    </div>
+  );
 };
-
 
 export default Task;
--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
 export const SHAPES = {
-    TRIANGLE: 'triangle',
-    SQUARE: 'square',
-    CIRCLE: 'circle',
-    STAR: 'star',
-}
+  TRIANGLE: 'triangle',
+  SQUARE: 'square',
+  CIRCLE: 'circle',
+  STAR: 'star',
+};
 
 export const COLORS = {
-    RED: 'red',
-    BLUE: 'blue',
-    ORANGE: 'orange',
-    GREEN: 'green',
-    WHITE: 'white',
-}
+  RED: 'red',
+  BLUE: 'blue',
+  ORANGE: 'orange',
+  GREEN: 'green',
+  WHITE: 'white',
+};
